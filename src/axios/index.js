3f87fb991b0a37416f51f4d6d1b605907233c1aa
Jang-Eun-Ji <125184448+Jang-Eun-Ji@users.noreply.github.com>
2024-03-12 14:53:16 +0900

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -5,7 +5,7 @@ const EXPIRATION_KEY = 'exp';
 
 // Axios 인스턴스 생성
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: `${process.env.VUE_APP_API_BASE_URL}`,
     headers: {
         'Content-Type': 'application/json',
     },
